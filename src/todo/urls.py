6734e85f2b6fe25953dd7ad653679dd83b7b441f
CTS\624604 <jeebitesh.kalantri@cognizant.com>
2018-06-04 14:22:12 +0200

    Project initial working setup completed

---
+++ src/todo/urls.py
@@ -0,0 +1,13 @@
+#from django.conf.urls import url, include
+from django.conf.urls import include, url
+from django.urls import path
+from todo import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r'todos', views.TodoItemViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
