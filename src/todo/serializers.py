6136a7f1fb62e39e3252c3d2dac9c5fb4c08cba3
CTS\624604 <jeebitesh.kalantri@cognizant.com>
2018-06-05 15:56:52 +0200

    adding missing code due to git reset hard

--- src/todo/serializers.py
+++ src/todo/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from todo.models import TodoItem
 
-class TodoItemSerializer(serializers.HyperlinkModelSrializer):
+class TodoItemSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.ReadOnlyField()
     class Meta:
         model = TodoItem
