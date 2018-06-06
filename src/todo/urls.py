3df6e59ed7e6a6d7652b322a8cbedd0ae2da064a
CTS\624604 <jeebitesh.kalantri@cognizant.com>
2018-06-06 08:50:11 +0200

    changes for test cases

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,4 +1,3 @@
-#from django.conf.urls import url, include
 from django.conf.urls import include, url
 from django.urls import path
 from todo import views
--- src/todo/views.py
+++ src/todo/views.py
@@ -8,15 +8,18 @@ from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
 # Create your views here.
+
+
 class TodoItemViewSet(viewsets.ModelViewSet):
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
 
     def perform_create(self, serializer):
         instance = serializer.save()
-        instance.url = reverse('todoitem-detail', args=[instance.pk], request=self.request)
+        instance.url = reverse(
+            'todoitem-detail', args=[instance.pk], request=self.request)
         instance.save()
 
     def delete(self, request):
         TodoItem.objects.all().delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)           
+        return Response(status=status.HTTP_204_NO_CONTENT)
